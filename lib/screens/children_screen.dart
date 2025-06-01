9c61bbc82a867f2bf59348d09acd89762b4e372c
XLianLZ <lianbox7304@gmail.com>
2025-05-31 23:30:20 -0500

    feat(children-screen): add children screen file

---
+++ lib/screens/children_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChildrenScreen extends StatelessWidget {
+  const ChildrenScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
